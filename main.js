607c6dd07082ca7a93876dcfdce93bfb52e4aac5
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2023-09-16 17:33:22 +0100

    refactored js code

--- main.js
+++ main.js
@@ -1,89 +1,157 @@
-let form = document.getElementById("toggle-content-form");
-let plan = document.getElementById("toggle-content-plan");
-let addons_monthly = document.getElementById("toggle-content-addons_monthly");
-let addons_yearly = document.getElementById("toggle-content-addons_yearly");
+const form = document.getElementById("toggle-content-form");
+const plan = document.getElementById("toggle-content-plan");
+const addons_monthly = document.getElementById("toggle-content-addons_monthly");
+const addons_yearly = document.getElementById("toggle-content-addons_yearly");
 
-let summary_monthly = document.getElementById("toggle-content-summary_monthly");
-let summary_yearly = document.getElementById("toggle-content-summary_yearly");
+const summary_monthly = document.getElementById(
+  "toggle-content-summary_monthly"
+);
+const summary_yearly = document.getElementById("toggle-content-summary_yearly");
 
-let confirmation = document.getElementById("toggle-content-confirmation");
-let attribution = document.getElementById("toggle-content-attribution");
-let goBackBtn = document.getElementById("go_back_button");
-let nextStepBtn = document.getElementById("next_step_button");
+const confirmation = document.getElementById("toggle-content-confirmation");
+const attribution = document.getElementById("toggle-content-attribution");
+const goBackBtn = document.getElementById("go_back_button");
+const nextStepBtn = document.getElementById("next_step_button");
 
-let monthly_plan = document.getElementById("toggle-content-monthly_plan");
-let yearly_plan = document.getElementById("toggle-content-yearly_plan");
-let switch_div = document.getElementById("plan_switch");
+const monthly_plan = document.getElementById("toggle-content-monthly_plan");
+const yearly_plan = document.getElementById("toggle-content-yearly_plan");
+const switch_div = document.getElementById("plan_switch");
 
-let btnDiv = document.getElementById("toggle-content-btn_div");
+const btnDiv = document.getElementById("toggle-content-btn_div");
+
+const mobileView = document.getElementsByClassName(
+  "toggle-content-steps_mobile"
+);
+
+//-----------------STEP NUMBER ICONS -----------------
 
 let currentPageIndex;
 
-let pageArray = [
+const pageArray = [
   form,
   plan,
   addons_monthly,
-  // addons_yearly,
   summary_monthly,
-  // summary_yearly,
   confirmation,
   attribution,
 ];
 
-// // ---REFACTORING---
-// make array of all the pages
-// write function that maps trough the array and hides/shows the neccery pages
+const stepNumbersArrray_color = [
+  "one_color",
+  "two_color",
+  "three_color",
+  "four_color",
+];
+const stepNumbersArrray = ["one", "two", "three", "four"];
 
-function nextStep() {
-  // Find the index of the currently visible div
-  let currentPageIndex = pageArray.findIndex((page) => {
-    return page.classList.contains("current-page");
-  });
-  console.log("currentPageIndex", currentPageIndex);
+const stepNumbersArrrayM = ["oneM", "twoM", "threeM", "fourM"];
+const stepNumbersArrray_colorM = [
+  "one_colorM",
+  "two_colorM",
+  "three_colorM",
+  "four_colorM",
+];
 
-  console.log("currentPage", pageArray[currentPageIndex + 1]);
+//----------------- HELPER FUNCTIONS -----------------
+function getCurrentPageIndex() {
+  return pageArray.findIndex((page) => page.classList.contains("current-page"));
+}
 
-  // Check if currentPageIndex is valid
-  if (currentPageIndex !== -1 && currentPageIndex < pageArray.length - 1) {
-    // Remove the current-page class from the current div
-    pageArray[currentPageIndex].classList.remove("current-page");
-    pageArray[currentPageIndex].style.display = "none";
-    // Add the current-page class to the next div
-    pageArray[currentPageIndex + 1].classList.add("current-page");
-    pageArray[currentPageIndex + 1].style.display = "block";
-  }
-
-  if (currentPageIndex === 0) {
-    goBackBtn.style.visibility = " visible";
-  }
-
-  if (currentPageIndex === pageArray.length - 4) {
+function toggleButtonVisibility() {
+  const currentPageIndex = getCurrentPageIndex();
+  goBackBtn.style.visibility = currentPageIndex !== 0 ? "visible" : "hidden";
+  if (currentPageIndex === pageArray.length - 3) {
     nextStepBtn.style.display = "none";
     confirm_step_button.style.display = "block";
+  } else {
+    nextStepBtn.style.display = "block";
+    confirm_step_button.style.display = "none";
   }
 }
 
-function goBack() {
-  let currentPageIndex = pageArray.findIndex((page) => {
-    return page.classList.contains("current-page");
-  });
-  console.log("currentPageIndex", currentPageIndex);
+// ----NEXT STEP----
+function nextStep() {
+  let currentPageIndex = getCurrentPageIndex();
   if (currentPageIndex !== -1 && currentPageIndex < pageArray.length - 1) {
-    // Remove the current-page class from the current div
     pageArray[currentPageIndex].classList.remove("current-page");
     pageArray[currentPageIndex].style.display = "none";
-    // Add the current-page class to the next div
+    pageArray[currentPageIndex + 1].classList.add("current-page");
+    pageArray[currentPageIndex + 1].style.display = "block";
+  }
+  toggleButtonVisibility();
+  showStepNumber();
+}
+
+// ----GO BACK A STEP----
+function goBack() {
+  let currentPageIndex = getCurrentPageIndex();
+  if (currentPageIndex !== -1 && currentPageIndex < pageArray.length - 1) {
+    pageArray[currentPageIndex].classList.remove("current-page");
+    pageArray[currentPageIndex].style.display = "none";
     pageArray[currentPageIndex - 1].classList.add("current-page");
     pageArray[currentPageIndex - 1].style.display = "block";
   }
-  if (currentPageIndex === 1) {
-    goBackBtn.style.visibility = " hidden";
+  toggleButtonVisibility();
+  showStepNumber();
+}
+// ----Numbers on the side----
+
+function showStepNumber() {
+  currentPageIndex = getCurrentPageIndex();
+  if (screen.width < 992) {
+    stepNumbersArrrayM.forEach((stepNumber, index) => {
+      const stepNumberElementM = document.getElementById(stepNumber);
+      if (index === currentPageIndex) {
+        stepNumberElementM.style.display = "none";
+      } else {
+        stepNumberElementM.style.display = "block";
+      }
+    });
+
+    stepNumbersArrray_colorM.forEach((stepNumber, index) => {
+      const stepNumberColorElementM = document.getElementById(stepNumber);
+      if (index === currentPageIndex) {
+        stepNumberColorElementM.style.display = "block";
+      } else {
+        stepNumberColorElementM.style.display = "none";
+      }
+    });
+  } else {
+    stepNumbersArrray.forEach((stepNumber, index) => {
+      const stepNumberElement = document.getElementById(stepNumber);
+      if (index === currentPageIndex) {
+        stepNumberElement.style.display = "none";
+      } else {
+        stepNumberElement.style.display = "block";
+      }
+      stepNumbersArrray_color.forEach((stepNumber, index) => {
+        const stepNumberColorElement = document.getElementById(stepNumber);
+        if (index === currentPageIndex) {
+          stepNumberColorElement.style.display = "block";
+        } else {
+          stepNumberColorElement.style.display = "none";
+        }
+      });
+    });
   }
+}
+// ----SHOW YEARLY PLAN OR MONTHLY PLAN----
+
+function showYearlyPlan() {
+  monthly_plan.style.display = switch_div.checked ? "none" : "block";
+  yearly_plan.style.display = switch_div.checked ? "block" : "none";
+
+  pageArray.splice(2, 2);
+  pageArray.splice(
+    2,
+    0,
+    switch_div.checked ? addons_yearly : addons_monthly,
+    switch_div.checked ? summary_yearly : summary_monthly
+  );
 }
 
 // ---CONFIRMATION PAGE---
 function Confirm() {
-  console.log("finish");
   summary_monthly.style.display = "none";
   summary_yearly.style.display = "none";
   confirmation.style.display = "block";
@@ -92,8 +160,8 @@ function Confirm() {
 }
 
 // ---CHANGE PLAN---
+
 function changePlan() {
-  console.log("change plan");
   if (switch_div.checked) {
     monthly_plan.style.display = "none";
     yearly_plan.style.display = "block";
@@ -101,6 +169,7 @@ function changePlan() {
   } else {
     plan.style.display = "block";
   }
+
   addons_monthly.style.display = "none";
   addons_yearly.style.display = "none";
   summary_monthly.style.display = "none";
@@ -109,140 +178,76 @@ function changePlan() {
   attribution.style.display = "none";
   nextStepBtn.style.display = "block";
   confirm_step_button.style.display = "none";
-}
 
-// ----SHOW YEARLY PLAN OR MONTHLY PLAN----
-function showYearlyPlan() {
-  if (switch_div.checked) {
-    monthly_plan.style.display = "none";
-    yearly_plan.style.display = "block";
-  } else {
-    monthly_plan.style.display = "block";
-    yearly_plan.style.display = "none";
-  }
+  pageArray[3].classList.remove("current-page");
+  pageArray[1].classList.add("current-page");
 }
 
 // ----ADD ONS----
+
 function planSelection(value) {
-  //can i make this all into one function?
-  let planChoise = value;
-  console.log(planChoise);
-  let existingContent = "";
+  const planChoice = value;
+  const existingContent = switch_div.checked ? "(yearly)" : "(monthly)";
 
-  if (switch_div.checked) {
-    existingContent = "(yearly)";
-  } else {
-    existingContent = "(monthly)";
-  }
-
-  let updatedContentM = planChoise + " " + existingContent;
+  const updatedContentM = `${planChoice} ${existingContent}`;
   document.getElementById("planChoiseM").textContent = updatedContentM;
 
-  let updatedContentY = planChoise + " " + existingContent;
+  const updatedContentY = `${planChoice} ${existingContent}`;
   document.getElementById("planChoiseY").textContent = updatedContentY;
 
-  console.log("updatedContent", updatedContentM);
-  console.log("updatedContent", updatedContentY);
+  const priceElementM = document.getElementById(`${value.toLowerCase()}PriceM`);
+  const priceM = priceElementM.textContent;
 
-  // Get the price from the selected button's h4 element
-  let priceElementM = document.getElementById(`${value.toLowerCase()}PriceM`);
-  let priceM = priceElementM.textContent;
-  console.log(priceM);
+  const priceElementY = document.getElementById(`${value.toLowerCase()}PriceY`);
+  const priceY = priceElementY.textContent;
 
-  let priceElementY = document.getElementById(`${value.toLowerCase()}PriceY`);
-  let priceY = priceElementY.textContent;
-  console.log(priceY);
-
-  // Update the price in the summary page
   document.getElementById("selectedPlanPriceM").textContent = priceM;
   document.getElementById("selectedPlanPriceY").textContent = priceY;
 }
 
-// let itemForm = document.getElementsByClassName("add-ons_monthly"); // getting the parent container of all the checkbox inputs
-// let checkBoxes = itemForm.querySelectorAll('input[type="checkbox"]'); // get all the check box
-
 let itemFormsM = document.getElementsByClassName("add-ons");
-// let itemFormsY = document.getElementsByClassName("add-ons_yearly");
-console.log("itemFormsM", itemFormsM);
-// let checkBoxesM = itemFormsM[0].querySelectorAll('input[type="checkbox"]');
-
-//---WHY DO I HAVE TO DO THIS TWICE?---WHY CAN'T I JUST DO ITEM FORMS AS A WHOLE?
 let checkBoxesM = itemFormsM[0].querySelectorAll('input[type="checkbox"]');
 let checkBoxesY = itemFormsM[1].querySelectorAll('input[type="checkbox"]');
-
-console.log("checkBoxesM", checkBoxesM);
-console.log("checkBoxesY", checkBoxesY);
 let addonChoise = [];
 
 function addonSelection(checkbox) {
+  const isYearly = switch_div.checked;
+  const itemForms = isYearly ? itemFormsM[1] : itemFormsM[0];
+  const checkBoxes = itemForms.querySelectorAll('input[type="checkbox"]');
+
   addonChoise = [];
-
-  // console.log('addon_price',addon_price);
-  // console.log('Cboxvalue',Cboxvalue);
-
-  checkBoxesM.forEach((item) => {
-    console.log("item", item);
-    // loop all the checkbox item
+  checkBoxes.forEach((item, index) => {
     if (item.checked) {
-      //if the check box is checked
-      let data = {
-        // create an object
+      const data = {
         item: item.value,
         pricedigit: parseFloat(item.dataset.price),
         pricestring: item.dataset.pricelabel,
       };
-      addonChoise.push(data); //stored the objects to result array
+
+      addonChoise.push(data);
+
+      const addonText = data.item;
+      const addonPriceString = data.pricestring;
+      const addonPrice = data.pricedigit;
+
+      const addonClassPrefix = isYearly ? ".addonChoiseY" : ".addonChoise";
+      const addonPriceClassPrefix = isYearly ? ".addonPriceY" : ".addonPrice";
+
+      document.querySelector(`${addonClassPrefix}${index + 1}`).textContent =
+        addonText;
+      document.querySelector(
+        `${addonPriceClassPrefix}${index + 1}`
+      ).textContent = addonPriceString;
+
+      let addonTotal = addonChoise.reduce(
+        (total, addon) => total + addon.pricedigit,
+        0
+      );
+
+      const totalElement = document.querySelector(
+        `.totalPrice${isYearly ? "Y" : "M"}`
+      );
+      totalElement.textContent = `$${addonTotal.toFixed(2)}/mo`;
     }
   });
-
-  checkBoxesY.forEach((item) => {
-    console.log("item", item);
-    // loop all the checkbox item
-    if (item.checked) {
-      //if the check box is checked
-      let data = {
-        // create an object
-        item: item.value,
-        pricedigit: parseFloat(item.dataset.price),
-        pricestring: item.dataset.pricelabel,
-      };
-      addonChoise.push(data); //stored the objects to result array
-    }
-  });
-
-  console.log("addonChoise", addonChoise);
-
-  let addonTotal = 0;
-  for (let i = 0; i < addonChoise.length; i++) {
-    let addonText = addonChoise[i].item;
-    let addon_price_string = addonChoise[i].pricestring;
-    let addon_price = addonChoise[i].pricedigit;
-
-    console.log("addonText", addonText);
-    console.log("addon_price_string", addon_price_string);
-    console.log("addon_price", addon_price);
-
-    if (addonText) {
-      if (switch_div.checked) {
-        document.querySelector(`.addonChoiseY${i + 1}`).textContent = addonText;
-        document.querySelector(`.addonPriceY${i + 1}`).textContent =
-          addon_price_string;
-        addonTotal += addon_price; // Accumulate the prices of selected add-ons
-
-        console.log("Total addon price:", addonTotal);
-
-        let totalElement = document.querySelector(".totalPriceY");
-        totalElement.textContent = `$+${addonTotal.toFixed(2)}/mo`;
-      } else {
-        document.querySelector(`.addonChoise${i + 1}`).textContent = addonText;
-        document.querySelector(`.addonPrice${i + 1}`).textContent =
-          addon_price_string;
-        addonTotal += addon_price; // Accumulate the prices of selected add-ons
-        console.log("Total addon price:", addonTotal);
-
-        let totalElement = document.querySelector(".totalPriceM");
-        totalElement.textContent = `$+${addonTotal.toFixed(2)}/mo`;
-      }
-    }
-  }
 }
